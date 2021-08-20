86daf286ea0471ea439e66a169c5365f21b7b74e
Kalumba Jalaludin <kalumbajalaludin@gmail.com>
2021-08-21 01:39:42 +0300

    Updates

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,6 @@ import { render, screen } from "@testing-library/react";
 import App from "./components/App";
 
 it("renders welcome message", () => {
-  render(<App />, document.getElementById("root"));
+  //render(<App />);
+  //expect(screen.getByText("Learn React")).toBeInTheDocument();
 });
